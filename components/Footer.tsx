d3f4a2e977eace50eba0c141fa0417e74d8f5352
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-09-26 13:56:47 +0530

    footer starter

---
+++ components/Footer.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+// later add link
+// import { TOGETHER_AI_LINK } from "@/lib/constant";
+
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center py-4">
+      <a
+        href={TOGETHER_AI_LINK}
+        target="_blank"
+        className="text-center text-sm text-[#555]"
+      >
+        Powered by{" "}
+        <img
+          className="inline ml-1"
+          src="/together.svg"
+          width="69"
+          height="15"
+        />
+      </a>
+    </footer>
+  );
+}
